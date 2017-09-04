6ca2be4240292ec2775a6b7adc5eb6f7657fe35a
klequis <klequis@gmail.com>
2017-09-04 12:16:53 -0700

    keep pokemon in box

--- app.js
+++ app.js
@@ -1,39 +1,16 @@
-// moveIt()
-
-
-
 function move2() {
-  // let x = document.getElementById('posX').value
-  // let y = document.getElementById('posY').value
-  // let elem = document.getElementById("myAnimation");
   let elem = document.getElementById("pokemon");
   let pos = 0
-  let id = setInterval(moveIt, 10)
-  function moveIt() {
-    if (pos < 500) {
+  let id1 = setInterval(moveIt1, 10)
+  function moveIt1() {
+    if (pos < 350) {
       pos++
-      console.log(pos)
       elem.style.top = pos + 'px';
       elem.style.left = pos + 'px';
+      console.log('top', elem.style.top)
+      console.log('left', elem.style.left)
     } else {
-      clearInterval(id)
-    }
-
-  }
-
-}
-
-function myMove() {
-  var elem = document.getElementById("myAnimation");
-  var pos = 0;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++;
-      elem.style.top = pos + 'px';
-      elem.style.left = pos + 'px';
+      clearInterval(id1)
     }
   }
 }
