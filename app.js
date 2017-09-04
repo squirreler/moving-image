18765cf9c63ec85a79d69ee89ea301c41d2b6347
daniel <danielb.tvc@gmail.com>
2017-09-04 15:39:50 -0700

    update

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 function move2() {
   let elem = document.getElementById("pokemon");
-  let pos = 0
+  let posL = 0
+  let posT = 0
   let id1 = setInterval(moveIt1, 10)
+
   function moveIt1() {
-    if (pos < 350) {
-      pos++
-      elem.style.top = pos + 'px';
-      elem.style.left = pos + 'px';
+    if (posL < 150 || posT < 100) {
+      posL++
+      posT++
+      if (posT < 100) {
+        elem.style.top = posT + 'px';
+      }
+      if (posL < 5700) {
+        elem.style.left = posL + 'px';
+      }
       console.log('top', elem.style.top)
       console.log('left', elem.style.left)
     } else {
@@ -15,6 +22,87 @@ function move2() {
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function p(txt) {
   document.write(`<p>${txt}</p>`)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// function move2() {
+//   let elem = document.getElementById("pokemon");
+//   let pos = 0
+//   let id1 = setInterval(moveIt1, 10)
+//   function moveIt1() {
+//     if (pos < 350) {
+//       pos++
+//       elem.style.top = pos + 'px';
+//       elem.style.left = pos + 'px';
+//       console.log('top', elem.style.top)
+//       console.log('left', elem.style.left)
+//     } else {
+//       clearInterval(id1)
+//     }
+//   }
+// }
+//
+// function p(txt) {
+//   document.write(`<p>${txt}</p>`)
+// }
